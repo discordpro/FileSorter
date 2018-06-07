928e6fd786d2160f01a9fb05876458d1c4ad58c7
discordpro <discordpro01@gmail.com>
2018-06-06 20:37:22 -0400

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+title App sorter setup
+cls
+echo Setting up in 3 seconds
+timeout /t 3
+md txt
+md exe
+md reg
+md aud
+md img
+md text
+title Setup done!
+title Completed
+pause
+echo Running ReadMe.txt in 10 seconds (OR PRESS ANY KEY TO RUN INSTANTLY)
+ReadMe.txt
